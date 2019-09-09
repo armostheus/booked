1e957eaf8a9845816c9b3f4cd574f256446d051c
Armostheus <shubham.chakraboty22@gmail.com>
2019-09-09 17:50:56 +0530

    Creating initial structure

---
+++ src/components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from './body/home/Home'
+
+class Body extends React.Component{
+    render(){
+        return(
+            <div>
+                <Home />
+            </div>
+        )
+    }
+}
+
+export default Body;
---
+++ src/components/body/home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Home extends React.Component{
+    render(){
+        return(
+            <div>
+                <h3>
+                    Hi {'{username}'}! here's your calendar.
+                </h3>
+            </div>
+        )
+    }
+}
+
+export default Home;
