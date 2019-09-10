782f7765255bdbc26424c5839876d47c1915cd83
Armostheus <shubham.chakraboty22@gmail.com>
2019-09-10 23:18:51 +0530

    Created List functional component

--- src/components/body/home/Home.js
+++ src/components/body/home/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Calendar from 'react-calendar'
 import './Home.css'
+
+import HomeBookingList from '../list/HomeBookingList'
 /* import Calendar from 'react-calendar/dist/entry.nostyle'
  */
 
@@ -15,18 +17,32 @@ class Home extends React.Component{
                     ({ date, view }) => view === 'month' && date.getDate() === 3 && date.getMonth() === 8 ? 'wednesday' : null    
                 }
                 />
-                <button>Create Personal Booking</button>
-                <button>Create Group Booking</button>
-                <button>Exit Booking</button>
-                <button>View Your Booking</button>
-                <button>Create Event</button>
-                <button>View Your Events</button>
-                <button>Create Group</button>
-                <button>View Group</button>
-                <button>Add Friend</button>
-                <button>View Friend</button>
-                <button>View Friend's Availability</button>
-                <button>View Group's Availability</button>
+                <br/>
+                <HomeBookingList />
+                <div className="flex-container">
+                    <button className="flex-content">Create Personal Booking</button>
+                    <button className="flex-content">Create Group Booking</button>
+                </div>
+                <div className="flex-container">
+                    <button className="flex-content">Exit Booking</button>
+                    <button className="flex-content">View Your Booking</button>
+                </div>
+                <div className="flex-container">
+                    <button className="flex-content">Create Event</button>
+                    <button className="flex-content">View Your Events</button>
+                </div>
+                <div className="flex-container">
+                    <button className="flex-content">Create Group</button>
+                    <button className="flex-content">View Group</button>
+                </div>
+                <div className="flex-container">
+                    <button className="flex-content">Add Friend</button>
+                    <button className="flex-content">View Friend</button>
+                </div>
+                <div className="flex-container">
+                    <button className="flex-content">View Friend's Availability</button>
+                    <button className="flex-content">View Group's Availability</button>
+                </div>
             </div>
         )
     }
---
+++ src/components/body/list/HomeBookingList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomeBookingList = (props) => {
+    return(
+        <div>
+            <h5>{'10:30 AM'} - {'05:30 PM'}</h5>
+            Going to party
+        </div>
+    )
+}
+
+export default HomeBookingList;
