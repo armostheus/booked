fb1407f69ba01727e4bfe1f744db10a97259ce46
Armostheus <shubham.chakraboty22@gmail.com>
2019-09-12 14:52:08 +0530

    Adding mock data

---
+++ src/mock-data/MockHomeData.js
@@ -0,0 +1,82 @@
+export const MonthData = [
+    {
+        id: 1,
+        date: new Date("2019-09-03"),
+        startTime: "04:00 PM",
+        endTime: "08:00 PM",
+        title: "Office Party",
+        booked_by: "Ajinkya Rahane"
+    },
+    {
+        id: 2,
+        date: new Date("2019-09-03"),
+        startTime: "04:00 AM",
+        endTime: "02:00 PM",
+        title: "Group Study",
+        booked_by: "Virat Kohli"
+    },
+    {
+        id: 3,
+        date: new Date("2019-09-04"),
+        startTime: "00:00 AM",
+        endTime: "11:59 PM",
+        title: "Chilling",
+        booked_by: "Shubham Chakraborty"
+    },
+    {
+        id: 4,
+        date: new Date("2019-09-05"),
+        startTime: "04:00 PM",
+        endTime: "08:00 PM",
+        title: "Office Party",
+        booked_by: "Tarenjit Kaur"
+    },
+    {
+        id: 5,
+        date: new Date("2019-09-11"),
+        startTime: "00:00 AM",
+        endTime: "11:59 PM",
+        title: "Mom's Birthday",
+        booked_by: "Shubham Chakraborty"
+    },
+    {
+        id: 6,
+        date: new Date("2019-09-23"),
+        startTime: "08:00 PM",
+        endTime: "11:59 PM",
+        title: "Night Out in Pawana Lake",
+        booked_by: "Chana Masala"
+    },
+    {
+        id: 7,
+        date: new Date("2019-09-24"),
+        startTime: "00:00 AM",
+        endTime: "08:00 PM",
+        title: "Night Out in Pawana Lake",
+        booked_by: "Chana Masala"
+    },
+    {
+        id: 8,
+        date: new Date("2019-09-27"),
+        startTime: "12:05 PM",
+        endTime: "03:00 PM",
+        title: "Online Test",
+        booked_by: "CPMT"
+    },
+    {
+        id: 9,
+        date: new Date("2019-09-30"),
+        startTime: "00:00 AM",
+        endTime: "12:00 PM",
+        title: "Test Booking",
+        booked_by: "Tester Terstee"
+    },
+    {
+        id: 10,
+        date: new Date("2019-09-30"),
+        startTime: "12:01 PM",
+        endTime: "11:59 PM",
+        title: "Test Booking",
+        booked_by: "Tester Terstee"
+    },
+];
