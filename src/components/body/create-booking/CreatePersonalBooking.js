b75c4278b0f7929f86ff3d5a08f5628d1122fab9
Armostheus <shubham.chakraboty22@gmail.com>
2019-09-13 18:50:42 +0530

    Developing personal booking view

--- src/components/body/create-booking/CreatePersonalBooking.js
+++ src/components/body/create-booking/CreatePersonalBooking.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import DatePicker from 'react-datepicker'
 
+import "react-datepicker/dist/react-datepicker.css";
+
 class CreatePersonalBooking extends React.Component{
 
     render(){
         return(
             <div>
                 <h3>CreatePersonalBooking</h3>
-                Date : <input type="date" name="booking"></input>
+                Date : <DatePicker /><br/>
+                Start Time: <DatePicker 
+                                showTimeSelect
+                                showTimeSelectOnly
+                                timeIntervals={15}
+                                timeCaption="Time"
+                                dateFormat="h:mm aa"
+                            /><br/>
+                End Time: <DatePicker 
+                                showTimeSelect
+                                showTimeSelectOnly
+                                timeIntervals={15}
+                                timeCaption="Time"
+                                dateFormat="h:mm aa"
+                            />
             </div>
         )
     }
