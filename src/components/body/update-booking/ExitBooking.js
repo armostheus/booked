2ed104087310a3ee399eefb537f0d3e0d05f4a45
Armostheus <shubham.chakraboty22@gmail.com>
2019-09-16 16:47:01 +0530

    Added mock data for exit Booking

--- src/components/body/update-booking/ExitBooking.js
+++ src/components/body/update-booking/ExitBooking.js
@@ -6,7 +6,9 @@ class ExitBooking extends React.Component{
     render(){
         return(
             <div>
-                ExitBooking
+                <h3>ExitBooking</h3>
+                Your Upcoming Bookings:
+                
             </div>
         )
     }
---
+++ src/mock-data/UpcomingBookingList.js
@@ -0,0 +1,29 @@
+export const UpcomingBookingList = [
+    {
+        bookingTitle : 'Cook Pizza',
+        date : new Date('2019-09-18'),
+        startTime : "5:15 AM",
+        endTime : "6:50 AM",
+        createdBy : "Ajinkya Rahane",
+        desc : "Cook pizza for the crow visiting home in morning",
+        members : []
+    },
+    {
+        bookingTitle : 'Playing Badminton',
+        date : new Date('2019-09-28'),
+        startTime : "9:15 AM",
+        endTime : "1:50 PM",
+        createdBy : "Gattu Gilari",
+        desc : "Playing Badminton with bois",
+        members : ["Gattu Gilari","Matru Bijli", "Raja Hindustani", "Pankaj Udas"]
+    },
+    {
+        bookingTitle : 'Motorcycle Ride',
+        date : new Date('2019-09-30'),
+        startTime : "00:00 AM",
+        endTime : "11:59 PM",
+        createdBy : "Rahul Sharma",
+        desc : "Full day ride to Mahabaleshwar and back",
+        members : ["Nana Patekar", "Rishabh Jambhulkar", "Dashy Buoy", "Kartar Singh"]
+    }
+]
