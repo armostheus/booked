aec71139aee99196acb8c0cde5f138e856bb6b8b
Armostheus <shubham.chakraboty22@gmail.com>
2019-09-17 16:39:41 +0530

    View Booking component intermediate commit and mock data creation

--- src/components/body/update-booking/ViewBooking.js
+++ src/components/body/update-booking/ViewBooking.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
 class ViewBooking extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            yourBooking : null,
+            othersBooking : null,
+            requests : null
+        }
+    }
     render(){
         return(
             <div>
-                ViewBooking
+                <div>
+                    <h3>ViewBooking</h3>
+                    <button>Your Bookings</button>
+                    <button>Others' Bookings</button>
+                    <button>Booking Request</button>
+                </div>
+                <div>
+
+                </div>
             </div>
         )
     }
---
+++ src/mock-data/ViewBookingData.js
@@ -0,0 +1,56 @@
+export const MyBookings = [
+    {
+        id : 1,
+        title: "Make Sandwich",
+        date : new Date("2019-11-19"),
+        startTime : "05:30 PM",
+        endTime : "11:30 PM",
+        otherMembers : [],
+        desc : "Make sandwich for 12 people with proper jam and butter",
+        reminder : "05:00 PM",
+    },
+    {
+        id : 2,
+        title: "Play Tennis",
+        date : new Date("2019-09-21"),
+        startTime : "12:00 PM",
+        endTime : "05:00 PM",
+        otherMembers : ["Parin Shah"],
+        desc : "Play Tennis for the TCS tournament",
+        reminder : null,
+    },
+    {
+        id : 3,
+        title: "Team Brunch",
+        date : new Date("2019-06-19"),
+        startTime : "11:00 AM",
+        endTime : "11:55 AM",
+        otherMembers : ["Joshila Patthar", "P V Sindhu", "Karmath Nirantar"],
+        desc : "Team Brunch at Hyderabadi house after a long time",
+        reminder : "10:30 AM",
+    },
+    {
+        id : 4,
+        title: "Work on Booked",
+        date : new Date("2019-07-19"),
+        startTime : "00:00 AM",
+        endTime : "11:59 PM",
+        otherMembers : [],
+        desc : "Work on module completion of Booked web app",
+        reminder : null,
+    },
+    {
+        id : 5,
+        title: "Take your dog out",
+        date : new Date("2019-08-19"),
+        startTime : "04:00 PM",
+        endTime : "04:30 PM",
+        otherMembers : ["Doggo DeDog"],
+        desc : "Take your dog out for a walk",
+        reminder : "03:45 PM",
+    }
+]
+
+export const OthersBookings = [
+    {}
+]
