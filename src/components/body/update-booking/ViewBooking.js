8e6b4a1e4196bea58e7f8e004a6f76581cd27850
Armostheus <shubham.chakraboty22@gmail.com>
2019-09-19 15:17:19 +0530

    developed others booking view

--- src/components/body/update-booking/ViewBooking.js
+++ src/components/body/update-booking/ViewBooking.js
@@ -14,8 +14,9 @@ const ViewYourBookings = (props) => {
 
 const ViewOthersBookings = (props) => {
     return(
-        <div onClick{() => props.displayOthersDetails(props.data)}>
-            sf
+        <div onClick={() => props.displayOthersDetails(props.data)}>
+            {props.data.date.getDate()} {month[props.data.date.getMonth()]}, {props.data.startTime} - {props.data.endTime} <br />
+            <h4>{props.data.title}</h4> {props.data.organizer}
         </div>
     )
 }
@@ -37,6 +38,17 @@ class ViewBooking extends React.Component{
                 otherMembers : [],
                 desc : null,
                 reminder : null,
+            },
+            selectedOB : {
+                id : null,
+                title: null,
+                date : new Date("1971-01-01"),
+                startTime : null,
+                endTime : null,
+                otherMembers : [],
+                organizer : null,
+                desc : null,
+                reminder : null,
             }
         };
         this.changeTab = this.changeTab.bind(this);
@@ -52,6 +64,10 @@ class ViewBooking extends React.Component{
         this.setState({selectedYB : view});
     }
 
+    displayOthersDetails(data){
+        this.setState({selectedOB : data});
+    }
+
     updateDisplayDetails(p){
         let currentView = [this.state.selectedYB.id, this.state.yourBooking[0].id, this.state.yourBooking[this.state.yourBooking.length-1].id];
             if(p==='p' && currentView[0] != currentView[1]){
@@ -62,6 +78,19 @@ class ViewBooking extends React.Component{
                 let prevData = this.state.yourBooking.filter(data => data.id === (currentView[0]+1));
                 console.log(prevData[0]);
                 this.setState({selectedYB : prevData[0]});
+            }       
+    }
+
+    updateOthersDisplayDetails(p){
+        let currentView = [this.state.selectedOB.id, this.state.othersBooking[0].id, this.state.othersBooking[this.state.othersBooking.length-1].id];
+            if(p==='p' && currentView[0] != currentView[1]){
+                let prevData = this.state.othersBooking.filter(data => data.id === (currentView[0]-1));
+                console.log(prevData[0]);
+                this.setState({selectedOB : prevData[0]});
+            } else if(p==='n'&&currentView[0] != currentView[2]){
+                let prevData = this.state.othersBooking.filter(data => data.id === (currentView[0]+1));
+                console.log(prevData[0]);
+                this.setState({selectedOB : prevData[0]});
             }       
     }
 
@@ -96,6 +125,19 @@ class ViewBooking extends React.Component{
                     {
                         this.state.othersBooking.map(data => <ViewOthersBookings key={data.id} data={data} displayOthersDetails={(data) => this.displayOthersDetails(data)}/>)
                     }
+                    <div style={this.state.selectedOB.title ? {display : 'block'} : {display : 'none'}}>
+                        Title : {this.state.selectedOB.title}, <br/>
+                        Date : {this.state.selectedOB.date.getDate()} {month[this.state.selectedYB.date.getMonth()]}<br/>
+                        Start Time : {this.state.selectedOB.startTime}<br/>
+                        End Time : {this.state.selectedOB.endTime}<br/>
+                        Other Members : {this.state.selectedOB.otherMembers.map(data => data+',')}<br/>
+                        Organizer : {this.state.selectedOB.organizer}<br/>
+                        Desc : {this.state.selectedOB.desc}<br/>
+                        Organizer : {this.state.selectedOB.organizer}<br/>
+                        Reminder : <br/>
+                        <button>Update</button> <button>Exit</button><br/>
+                        <button onClick={()=>this.updateOthersDisplayDetails('p')}>previous</button> <button onClick={()=>this.updateOthersDisplayDetails('n')}>next</button>
+                    </div>
 
                 </div>
                 <div style={this.state.selectedTab === "BR" ? {display : 'block'} : {display : 'none'}}>
