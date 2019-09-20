784cfab33a65c5a026e95097b703ddad275bc0f8
Armostheus <shubham.chakraboty22@gmail.com>
2019-09-20 19:03:25 +0530

    intermediate commit

--- src/components/body/group/CreateGroup.js
+++ src/components/body/group/CreateGroup.js
@@ -3,7 +3,14 @@ import React from 'react'
 class CreateGroup extends React.Component{
     render(){
         return(
-            <div>CreateGroup</div>
+            <div>
+                <h3>CreateGroup</h3>
+                Group Name : <br/>
+                Group Friends : <br/>
+                Add Friend : <br/>
+                Remove Friend : <br/>
+                <button>Create Group</button>
+            </div>
         )
     }
 }
