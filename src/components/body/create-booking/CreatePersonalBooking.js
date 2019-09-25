4dc0203e9d524e417bd049da8f8fb5fc0c884139
Armostheus <shubham.chakraboty22@gmail.com>
2019-09-25 20:28:15 +0530

    CreatePersonalBooking JSON creation and state management

--- src/components/body/create-booking/CreatePersonalBooking.js
+++ src/components/body/create-booking/CreatePersonalBooking.js
@@ -2,28 +2,45 @@ import React from 'react'
 import DatePicker from 'react-datepicker'
 
 import "react-datepicker/dist/react-datepicker.css";
+import { setDate } from 'date-fns';
 
 class CreatePersonalBooking extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            data : {
                 date : null,
                 startTime : null,
                 endTime : null,
                 title : null,
+                private : false,
                 description : null,
-                reminder : null,
+                reminder : false,
+                reminderTime : null
             }
-        }
+        this.setData = this.setData.bind(this);
+        this.bookItBabe = this.bookItBabe.bind(this);
+    }
+
+    setData(prp){
+        this.setState(prp);
+    }
+
+    bookItBabe(){
+        console.log(this.state);
+        //Implement axios call to backend here
     }
 
     render(){
         return(
             <div>
                 <h3>CreatePersonalBooking</h3>
-                Date : <DatePicker /><br/>
+                Date : <DatePicker 
+                        selected={this.state.date}
+                        onChange={date => this.setData({date:date})}
+                    /><br/>
                 Start Time: <DatePicker 
+                                selected={this.state.startTime}
+                                onChange={date => this.setData({startTime:date})}
                                 showTimeSelect
                                 showTimeSelectOnly
                                 timeIntervals={15}
@@ -31,6 +48,8 @@ class CreatePersonalBooking extends React.Component{
                                 dateFormat="h:mm aa"
                             /><br/>
                 End Time: <DatePicker 
+                                selected={this.state.endTime}
+                                onChange={date => this.setData({endTime:date})}
                                 showTimeSelect
                                 showTimeSelectOnly
                                 timeIntervals={15}
@@ -38,32 +57,39 @@ class CreatePersonalBooking extends React.Component{
                                 dateFormat="h:mm aa"
                             />
                 <br />
-                Booking Title: <input type="text" name="title" /> <button>Event</button>
+                {/*Implement Throttling for booking title*/}
+                Booking Title: <input type="text" name="title" value={this.state.title} onChange={(e)=>this.setData({title:e.target.value})}/> <button>Event</button>
                 <br />
-                <input type="radio" id="public" name="visiblity" value="public" checked />
+                <input type="radio" id="public" name="visiblity" value="public" checked={!this.state.private} onChange={()=>this.setData({private:false})}/>
                 <label htmlFor="public">Public</label>
-                <input type="radio" id="private" name="visiblity" value="private" />
+                <input type="radio" id="private" name="visiblity" value="private" checked={this.state.private} onChange={()=>this.setData({private:true})}/>
                 <label htmlFor="private">Private</label>
                 <br />
-                Booking Desc: <textarea id="bookingDesc" name="bookingDesc" rows="5">
-                                A short description about your booking
+                Booking Desc: <textarea id="bookingDesc" name="bookingDesc" rows="5"
+                                placeholder="A short description about your booking"
+                                value={this.state.description}
+                                onChange={(e)=>this.setData({description:e.target.value})}
+                                >
                               </textarea>
                 <br />
-                Want Reminder? :<input type="radio" id="yes" name="reminder" value="yes" checked />
+                Want Reminder? :<input type="radio" id="yes" name="reminder" value="yes" checked={this.state.reminder} onChange={(e)=>this.setData({reminder:true})}/>
                                 <label htmlFor="reminder">Yes</label>
-                                <input type="radio" id="no" name="reminder" value="no" />
+                                <input type="radio" id="no" name="reminder" value="no" checked={!this.state.reminder} onChange={(e)=>this.setData({reminder:false})}/>
                                 <label htmlFor="reminder">No</label> 
                 <br />   
                 Reminder Time :  <DatePicker
                                     showTimeSelect
                                     showTimeSelectOnly
+                                    disabled={!this.state.reminder}
+                                    selected={this.state.reminderTime}
+                                    onChange={data=>this.setData({reminderTime:data})}
                                     timeFormat="HH:mm"
                                     timeIntervals={15}
                                     timeCaption="time"
-                                    dateFormat="MMMM d, yyyy h:mm aa"
+                                    dateFormat="h:mm aa"
                                     />
                 <br/>
-                <button>Book!</button>         
+                <button onClick={this.bookItBabe}>Book!</button>         
             </div>
         )
     }
