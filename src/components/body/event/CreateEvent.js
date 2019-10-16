c2db536bbc42a26e4951193f42b96e7022571c31
Armostheus <shubham.chakraboty22@gmail.com>
2019-10-16 18:30:43 +0530

    developing Create Event

--- src/components/body/event/CreateEvent.js
+++ src/components/body/event/CreateEvent.js
@@ -1,16 +1,43 @@
 import React from 'react'
 
 class CreateEvents extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            title : null,
+            description : null,
+            event : null
+        }
+        this.changeDescription = this.changeDescription.bind(this)
+        this.changeEvent = this.changeEvent.bind(this)
+        this.changeTitle = this.changeTitle.bind(this)
+        this.createEvent = this.createEvent.bind(this)
+    }
+
+    changeDescription(e){
+        this.setState({description : e.target.value})
+    }
+    changeEvent(e){
+        this.setState({event : e.target.value})
+    }
+    changeTitle(e){
+        this.setState({title : e.target.value})
+    }
+    createEvent(){
+        console.log(this.state)
+    }
+
+
     render(){
         return(
             <div>
                 <h3>Create New Event</h3>
-                Title : <input type="text" name="title" /><br/>
-                Description : <textarea id="eventDesc" name="eventDesc" rows="5">
-                                A short description of your event
+                Title : <input type="text" name="title" onChange={this.changeTitle}/><br/>
+                Description : <textarea id="eventDesc" name="eventDesc" rows="5" onChange={this.changeDescription} placeholder={'A short description of your event'}>
+                                
                               </textarea> <br/>
-                Event Name : <input type="text" name="title" /><br/>    
-                <button>Create Event</button>           
+                Event Name : <input type="text" name="title"  onChange={this.changeEvent}/><br/>    
+                <button onClick={this.createEvent}>Create Event</button>           
             </div>
         )
     }
